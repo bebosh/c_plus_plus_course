2f98952f59b888cc9acc6a7ec8b82a407c930eea
Paolo <pieffelleo@gmail.com>
2021-04-07 19:26:47 +0200

    hello world

---
+++ variable_fun/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int myInt;
+  myInt =15;
+  double myDouble = 12.24;
+  std::cout << myInt << endl;
+  std::cout << myDouble << endl;
+  return 0;
+}
